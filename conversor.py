89013be3057359e322b4c01ab8047ee8e70766e6
Jose Abzum <jose.abzum@escala24x7.com>
2021-04-18 00:58:35 -0600

    JA - some new features in conversor

--- conversor.py
+++ conversor.py
@@ -1,10 +1,47 @@
-pesos = input("Cuántos Quetzales tienes?: ")
-pesos = float(pesos)
+menu = """
+Bienvenido al conversor de monedas ❤
 
-valor_dolar = 7.7
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
+1 - Quetzales
+2 - Pesos Argentinos
+3 - Pesos mexicanos
 
-dolares = str(dolares)
+Elige una opción: """
 
-print("Tienes $"+dolares+ " dolares")
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos = input("Cuántos Quetzales tienes?: ")
+    pesos = float(pesos)
+
+    valor_dolar = 7.7
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+
+    dolares = str(dolares)
+
+    print("Tienes $"+dolares+ " dolares")
+elif opcion == 2:
+    pesos = input("Cuántos pesos argentinos tienes?: ")
+    pesos = float(pesos)
+
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+
+    dolares = str(dolares)
+
+    print("Tienes $"+dolares+ " dolares")
+elif opcion ==3:
+    pesos = input("Cuántos pesos mexicanos tienes tienes?: ")
+    pesos = float(pesos)
+
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+
+    dolares = str(dolares)
+
+    print("Tienes $"+dolares+ " dolares")
+else:
+    print("Ingrese una opción valida")
+
