017802e7b10fccb7500b84fe89cb4ab3729c47aa
Jose Abzum <jose.abzum@escala24x7.com>
2021-04-18 15:53:07 -0600

    JA - using functions

---
+++ funciones.py
@@ -0,0 +1,24 @@
+""" def imprimir_mensaje():
+    print("Mensaje especial")
+    print("usando func xd")
+
+imprimir_mensaje()
+imprimir_mensaje()
+imprimir_mensaje()
+imprimir_mensaje()
+"""
+
+def conver(mensaje):
+    print("Hola")
+    print("Como estas?")
+    print(mensaje)
+    print("adios")
+
+option = int(input("Elige una opción (1,2,3): "))
+
+if option == 1:
+    conver("Elegiste la opción 1")
+elif option == 2:
+    conver("Elegiste la opción 2")
+else:
+    print("la ca")
